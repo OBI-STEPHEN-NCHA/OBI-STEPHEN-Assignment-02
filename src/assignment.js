7b17ddb28ec0cdd4fc9a3a65bb825cfdd5539b3f
OBI-STEPHEN-NCHA <109057284+OBI-STEPHEN-NCHA@users.noreply.github.com>
2022-07-13 11:41:46 +0100

    OBI-STEPHEN-NCHA Assignments

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,11 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let sum=0;
+    for(let i=0; i<arrayOfNumbers.length; i++){sum=sum+arrayOfNumbers[i];}
+    return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+ assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +30,11 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let count=0;
+    for(let i=0; i<arrayOfNumbers.length; i++){ count=count+arrayOfNumbers[i];}
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+    return count;    
+    assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
